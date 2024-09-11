085a498a639749216a9f44950ce52846c8bf3370
DrPrime01 <drprime.dev@gmail.com>
2024-09-11 23:51:33 +0100

    project setup and dependencies installations

---
+++ app/layout.js
@@ -0,0 +1,37 @@
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const fontSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "CarePulse",
+  description: "A healthcare management system",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "nin-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
